55f2ff05f2a4855fc5523f54b1ec5771461b8371
Ghias Dahdouh <ghaisdahdouh@gmail.com>
2022-06-17 13:24:26 +0200

    Added_Students_To_About_Page

---
+++ src/Components/Student.js
@@ -0,0 +1,34 @@
+import styles from './Student.module.css'
+import CustomLink from "./CustomLink";
+
+export const Student = (props) => {
+    return (
+
+        <article className={styles.team__member}>
+    <div className={styles['team__member-image']}>
+      <img src={props.src} alt="Student" />
+    </div>
+    <div className={styles['team__member-info']}>
+      <h4>{props.studentName}</h4>
+      <p>{props.smallInfo}</p>
+    </div>
+    <div className={styles['team__member-socials']}>
+      <CustomLink to="/About/@Name">
+        {" "}
+        <i className={`bi bi-file-earmark-person`}></i>
+      </CustomLink>
+      <a href="https://linkedin.com" target="_blank" rel='noreferrer'>
+        {" "}
+        <i className={`bi bi-linkedin`}></i>
+      </a>
+      <a href={props.gitAccount} target="_blank"  rel='noreferrer'>
+        {" "}
+        <i className={`bi bi-github`}></i>
+      </a>
+      <CustomLink to="/About/@Name" className={`btn btn-primary align_btns`}>
+        More
+      </CustomLink>
+    </div>
+  </article>
+      );
+};
