bf7b27282f05f86b3aebab76cabbb2c641c8f0d1
Ghias Dahdouh <ghaisdahdouh@gmail.com>
2022-08-03 09:57:39 +0200

    Fixed Faqs

--- src/Components/Faq.js
+++ src/Components/Faq.js
@@ -1,35 +1,31 @@
-import {useState} from 'react';
-import styles from './Faq.module.css'
+import { useState } from "react";
+import styles from "./Faq.module.css";
 
+const Faq = ({ headerText, paragraphText }) => {
+  const [faqIconClassNames, SetFaqIconClassNames] =
+    useState("bi bi-plus-square");
+  const [displayMode, setDisplayMode] = useState("d-none");
 
-let displayMode  = "d-none";
-
-
-const Faq= ({headerText, paragraphText}) => {
-    const [faqIconClassNames, SetFaqIconClassNames] = useState('bi bi-plus-square');
-    const SetCssClasses = () =>{
-        if (faqIconClassNames === "bi bi-plus-square")
-        {
-            SetFaqIconClassNames("bi bi-dash-square") ;
-            displayMode = "d-inline-block";
-        }
-        else
-        {
-            SetFaqIconClassNames("bi bi-plus-square");
-            displayMode = "d-none";
-        }
+  const SetCssClasses = () => {
+    if (faqIconClassNames === "bi bi-plus-square") {
+      SetFaqIconClassNames("bi bi-dash-square");
+      setDisplayMode("d-inline-block");
+    } else {
+      SetFaqIconClassNames("bi bi-plus-square");
+      setDisplayMode("d-none");
     }
-    return(
-        <article className={styles.faq}>
-        <div onClick={SetCssClasses} className={styles.faq__icon}><i className={faqIconClassNames}></i></div>
-        <div className={styles.question__answer}>
-            <h4>{headerText}</h4>
-            <p className={displayMode}>
-                {paragraphText}
-            </p>
-        </div>
-    </article>   
-    );
-}
+  };
+  return (
+    <article onClick={SetCssClasses} className={styles.faq}>
+      <div className={styles.faq__icon}>
+        <i className={faqIconClassNames}></i>
+      </div>
+      <div className={styles.question__answer}>
+        <h4>{headerText}</h4>
+        <p className={displayMode}>{paragraphText}</p>
+      </div>
+    </article>
+  );
+};
 
 export default Faq;
